5d6e7adb03fbf3d44433d51023d785dda19a8e2f
Bob <bob.e.baxter@gmail.com>
2019-06-24 20:28:17 -0500

    auth wip

--- src/App/App.js
+++ src/App/App.js
@@ -5,13 +5,29 @@ import Home from '../components/Home/Home';
 
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <Auth />
-      <Home />
-    </div>
-  );
+import fbConnection from '../helpers/data/connection';
+
+fbConnection();
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  render() {
+    const loadComponent = () => {
+      if (this.state.authed) {
+        return <Home />;
+      }
+      return <Auth />;
+    };
+
+    return (
+      <div className="App">
+        {loadComponent()}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 import './Auth.scss';
 
 class Auth extends React.Component {
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
     return (
       <div className="Auth">
         <h1>Auth</h1>
+        <button className="btn btn-primary" onClick={this.loginClickEvent}>Login with Google</button>
       </div>
     );
   }
