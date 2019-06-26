fa1105bd604580887aa94ab9177c6eb1081decd7
Bob <bob.e.baxter@gmail.com>
2019-06-25 20:09:55 -0500

    fish inventory connected

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 import './Inventory.scss';
 
+import fishData from '../../helpers/data/fishData';
+
 class Inventory extends React.Component {
+  state = {
+    fishes: [],
+  }
+
+  componentDidMount() {
+    fishData.getFishes()
+      .then(fishes => this.setState({ fishes }))
+      .catch(err => console.error('could not get fishes', err));
+  }
+
   render() {
     return (
       <div className="Inventory">
